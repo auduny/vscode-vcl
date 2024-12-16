23e5d0efa348ab676a3115df4650dd06bc6539e9
Audun Ytterdal <audun@ytterdal.net>
2024-12-16 18:09:10 +0100

    Adding debugpath as well

--- src/extension.ts
+++ src/extension.ts
@@ -12,6 +12,7 @@ let client: lsp.LanguageClient;
 export async function activate(context: vscode.ExtensionContext) {
   const config = vscode.workspace.getConfiguration('varnishls');
   let serverPath = "";
+  let serverDebugPath = "";
   if (config.get('path')) {
     serverPath = config.get('path')
     log.warn("Custom varnishls set to: " + serverPath);
@@ -24,20 +25,35 @@ export async function activate(context: vscode.ExtensionContext) {
     : path.resolve(__dirname, '../../target/release/varnishls');
   }
 
+  if (config.get('debugPath')) {
+    serverDebugPath = config.get('debugPath');
+    log.warn("Custom varnishls debug path set to: " + serverDebugPath);
+  } else {
+    serverDebugPath = serverPath;
+  }
+
+  const homeDir = process.env.HOME || process.env.USERPROFILE; // Fallback for Windows
+  const env = Object.assign({}, process.env, {
+    VARNISHLS_VCC_PATHS: path.join(homeDir,'.vcc-files')
+  });
+
+
   const serverExecutable: lsp.Executable = {
 //    command: '/Users/ay/src/private/vscode/varnishls/target/debug/varnishls',
 //    command: '/Users/ay/src/private/vscode/varnishls/target/release/varnishls',
 //    command: __dirname + '/lsp/bin/varnishls-darwin-x86_64',
     command: serverPath,
-    args: ['lsp','--stdio']
+    args: ['lsp','--stdio'],
+    options: { env }
   };
 
   const serverDebugExecutable: lsp.Executable = {
     //    command: '/Users/ay/src/private/vscode/varnishls/target/debug/varnishls',
     //    command: '/Users/ay/src/private/vscode/varnishls/target/release/varnishls',
     //    command: __dirname + '/lsp/bin/varnishls-darwin-x86_64',
-        command: serverPath,
-        args: ['lsp','--stdio', '--debug']
+        command: serverDebugPath,
+        args: ['lsp','--stdio', '--debug'],
+        options: { env }
       };
 
   const serverOptions: lsp.ServerOptions = {
